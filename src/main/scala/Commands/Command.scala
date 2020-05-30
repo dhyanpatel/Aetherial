b195a4885d9dc7c004dd22e819d9fb339e9904a9
Dhyan <dhyanpp@gmail.com>
2020-05-30 02:24:23 -0500

    Arrays to Seqs

--- src/main/scala/Commands/Command.scala
+++ src/main/scala/Commands/Command.scala
@@ -2,7 +2,7 @@ package Commands
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 
 abstract class Command() {
-  var aliases : Array[String]
+  var aliases : Seq[String]
   var name : String
   def execute(event: MessageReceivedEvent)
 }
