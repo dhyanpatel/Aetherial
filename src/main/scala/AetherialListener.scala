3fb46283ad4a98d404f75c219ba9c47097dc537d
Dhyan <dhyanpp@gmail.com>
2020-05-30 02:41:52 -0500

    temporary command mapping concatenation

--- src/main/scala/AetherialListener.scala
+++ src/main/scala/AetherialListener.scala
@@ -1,11 +1,15 @@
+import Commands.About.Help
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 import Commands.Birthdays.SetBirthday
 
 class AetherialListener(val prefix: String) extends ListenerAdapter {
 
-  private val commands = SetBirthday.aliases.map(prefix + _ -> SetBirthday).toMap
 
+  private val commands = (
+    SetBirthday.aliases.map(prefix + _ -> SetBirthday).toMap
+      ++ Help.aliases.map(prefix + _ -> Help).toMap
+    )
 
   override def onMessageReceived(event: MessageReceivedEvent): Unit = {
     commands.get(event.getMessage.getContentRaw.split("\\s").head).foreach(_.execute(event))
