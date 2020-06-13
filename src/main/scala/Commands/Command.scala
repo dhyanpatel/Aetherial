7d6bdae2955be9132bf15be93723bfa4afd4c1b2
Dhyan <dhyanpp@gmail.com>
2020-06-13 04:30:40 -0500

    Update Command.scala

--- src/main/scala/Commands/Command.scala
+++ src/main/scala/Commands/Command.scala
@@ -6,5 +6,6 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 abstract class Command() {
   var aliases : Seq[String]
   var name : String
-  def execute(event: MessageReceivedEvent, transactor: Resource[IO, HikariTransactor[IO]])
+  def execute(event: MessageReceivedEvent, transactor: Resource[IO, HikariTransactor[IO]]) : Unit
+  def validateCommand(event: MessageReceivedEvent) : Boolean
 }
