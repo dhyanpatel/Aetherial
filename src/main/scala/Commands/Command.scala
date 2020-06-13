31465bdaca9cba4562821e6a1539327a144d6c91
Dhyan <dhyanpp@gmail.com>
2020-06-13 03:10:50 -0500

    doobie transactor for commands

--- src/main/scala/Commands/Command.scala
+++ src/main/scala/Commands/Command.scala
@@ -1,8 +1,10 @@
 package Commands
+import cats.effect.{IO, Resource}
+import doobie.hikari.HikariTransactor
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 
 abstract class Command() {
   var aliases : Seq[String]
   var name : String
-  def execute(event: MessageReceivedEvent)
+  def execute(event: MessageReceivedEvent, transactor: Resource[IO, HikariTransactor[IO]])
 }
