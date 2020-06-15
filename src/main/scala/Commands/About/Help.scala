7d72569e4c8fabef248e4ddeb64101c37ac9fade
Nikhil Prasad <nprasad493@gmail.com>
2020-06-15 01:23:27 -0500

    Basic help command (#6)
    "Finalized" help command that shows the available commands, not the sub commands. The message output by the help command will keep growing.

---
+++ src/main/scala/Commands/About/Help.scala
@@ -0,0 +1,25 @@
+package Commands.About
+
+import Commands.{Command, SubCommand}
+import cats.effect.{IO, Resource}
+import doobie.hikari.HikariTransactor
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent
+
+object Help extends Command with SubCommand {
+  override val aliases: Seq[String] = Seq("help")
+  override val name: String = "Help"
+
+  override def getSubCommand(event: MessageReceivedEvent): SubCommand = {
+    Help
+  }
+
+  override def execute(event: MessageReceivedEvent, transactor: Resource[IO, HikariTransactor[IO]]): Unit = {
+    event.getChannel.sendMessage(
+      "Available Commands: \n" +
+        "Birthday commands - Type !birthday to view available birthday commands \n").queue()
+  }
+
+  override def validateCommand(event: MessageReceivedEvent): Boolean = {
+    true
+  }
+}
