1a439dcd5e331395b7ce6e4d537cc965a05676ca
Nikhil Prasad <nprasad493@gmail.com>
2020-06-15 01:12:56 -0500

    Update Help.scala

--- src/main/scala/Commands/About/Help.scala
+++ src/main/scala/Commands/About/Help.scala
@@ -3,26 +3,22 @@ package Commands.About
 import Commands.{Command, SubCommand}
 import cats.effect.{IO, Resource}
 import doobie.hikari.HikariTransactor
-import doobie.util.transactor.Transactor
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 
-object Help extends Command with SubCommand{
+object Help extends Command with SubCommand {
   override val aliases: Seq[String] = Seq("help")
   override val name: String = "Help"
 
   override def getSubCommand(event: MessageReceivedEvent): SubCommand = {
-    event.getMessage.getContentRaw.split("\\s+").lift(1) match{
-      case None => Help
-      case Some(value) => {
-        case _ => Help
-      }
+    event.getMessage.getContentRaw.split("\\s+").lift(1) match {
+      case _ => Help
     }
   }
 
   override def execute(event: MessageReceivedEvent, transactor: Resource[IO, HikariTransactor[IO]]): Unit = {
     event.getChannel.sendMessage(
       "Available Commands: \n" +
-      "Birthday commands - Type .birthday to view available birthday commands \n").queue()
+        "Birthday commands - Type !birthday to view available birthday commands \n").queue()
   }
 
   override def validateCommand(event: MessageReceivedEvent): Boolean = {
