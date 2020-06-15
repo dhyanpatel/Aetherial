a9ea81a68cbd2a96e60306a8d36de7430f9abfc6
shashankv327 <shashank.vasanth@gmail.com>
2020-06-15 02:41:38 -0400

    Ping Command

--- src/main/scala/AetherialListener.scala
+++ src/main/scala/AetherialListener.scala
@@ -1,5 +1,6 @@
 import Commands.About.Help
 import Commands.Birthday.Birthday
+import Commands.Ping.Ping
 import cats.effect.{IO, Resource}
 import doobie.hikari.HikariTransactor
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
@@ -9,8 +10,9 @@ class AetherialListener(val prefix: String, val transactor: Resource[IO, HikariT
   extends ListenerAdapter {
 
   private val commands = (Birthday.aliases.map(prefix + _ -> Birthday).toMap
-  ++ Help.aliases.map(prefix + _ -> Help).toMap
-  )
+    ++ Help.aliases.map(prefix + _ -> Help).toMap
+    ++ Ping.aliases.map(prefix + _ -> Ping).toMap
+    )
 
   override def onMessageReceived(event: MessageReceivedEvent): Unit = {
     commands.get(event.getMessage.getContentRaw.split("\\s").head)
---
+++ src/main/scala/Commands/Ping/Ping.scala
@@ -0,0 +1,24 @@
+package Commands.Ping
+
+import Commands.{Command, SubCommand}
+import cats.effect.{IO, Resource}
+import doobie.hikari.HikariTransactor
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent
+
+object Ping extends Command with SubCommand {
+  override val aliases: Seq[String] = Seq("ping")
+  override val name: String = "Ping"
+
+  override def getSubCommand(event: MessageReceivedEvent): SubCommand = {
+    Ping
+  }
+
+  override def execute(event: MessageReceivedEvent, transactor: Resource[IO, HikariTransactor[IO]]): Unit = {
+    event.getChannel.sendMessage(
+      "Pong \n").queue()
+  }
+
+  override def validateCommand(event: MessageReceivedEvent): Boolean = {
+    true
+  }
+}
